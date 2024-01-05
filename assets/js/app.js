e7c217b6f7dc5dc0ee6b1cb62f54efc676f4af23
Emil410 <gasanovemil13@gmail.com>
2024-01-05 12:36:05 +0400

    checking

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+var words = ['build', 'work'];
+var currentIndex = 0;
+
+function animateText() {
+    var animatedText = $('#animatedText');
+    animatedText.fadeOut(500, function() {
+        animatedText.text(words[currentIndex]);
+        currentIndex = (currentIndex + 1) % words.length;
+        animatedText.fadeIn(500);
+    });
+}
+setInterval(animateText, 3000);

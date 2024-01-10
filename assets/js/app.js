ff4631cf1f02ddac2e01da5611e9ca46c5dd1a25
Emil410 <gasanovemil13@gmail.com>
2024-01-10 16:28:57 +0400

    finished

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,12 +1,22 @@
-var words = ['build', 'work'];
-var currentIndex = 0;
+// var words = ['build', 'work'];
+// var currentIndex = 0;
 
-function animateText() {
-    var animatedText = $('#animatedText');
-    animatedText.fadeOut(500, function() {
-        animatedText.text(words[currentIndex]);
-        currentIndex = (currentIndex + 1) % words.length;
-        animatedText.fadeIn(500);
-    });
-}
-setInterval(animateText, 3000);
+// function animateText() {
+//     var animatedText = $('#animatedText');
+//     animatedText.fadeOut(500, function() {
+//         animatedText.text(words[currentIndex]);
+//         currentIndex = (currentIndex + 1) % words.length;
+//         animatedText.fadeIn(500);
+//     });
+// }
+// setInterval(animateText, 3000);
+var swiper = new Swiper(".mySwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
